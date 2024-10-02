24a49ed977fcf6ca2809741983b1e7160b91adcc
Phantem717 <pradahsuherli@gmail.com>
2024-10-02 20:17:42 +0700

    Category Card Added

---
+++ resources/views/components/category-card.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+<body>
+<div class="d-flex flex-wrap justify-content-center align-items-center flex-column">
+    <div class="Category">
+    <div class="w-full h-full  ">
+    <img src="{{Vite::asset('resources/Images/Laravel.png')}}" alt="" class="CategoryImage">
+   
+    </div>
+    </div>
+
+    <div class="CategoryText">
+        <div class="w-50 d-flex items-center">
+        Front End Developer
+        </div>
+       
+    </div>
+    </div>
+  
+</body>
+</html>
